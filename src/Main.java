3b53a1ccfa3a84faa64c2696585672fd894d4e2f
bnurdogru <busranurdogru2@gmail.com>
2025-03-17 22:59:29 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Değişkenleri oluştur
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        // Scanner sınıfımızı tanımladık
+        Scanner inp = new Scanner(System.in);
+
+        // Kullanıcıdan değerlri al
+        System.out.print("Matematik Notunuz:");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz:");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz:");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe Notunuz:");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz:");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuz:");
+        muzik = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        int ortalama = toplam / 6;
+        System.out.println("Ortalamanız: " + ortalama);
+
+
+        String sonuc = ortalama > 60 ? "geçti" : " kaldı";
+
+        System.out.println(sonuc);
+
     }
 }
